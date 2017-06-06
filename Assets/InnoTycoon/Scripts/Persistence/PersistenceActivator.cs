81113dfed42d3788d6193046d09d9180ab12dac5
dadlo <dadlobr@gmail.com>
2017-06-06 18:38:58 -0300

    Add confirmation on new game to reset all data if there is a save ok dialog if there is no data to load from load game

--- Assets/InnoTycoon/Scripts/Persistence/PersistenceActivator.cs
+++ Assets/InnoTycoon/Scripts/Persistence/PersistenceActivator.cs
@@ -5,17 +5,21 @@ using UnityEngine.UI;
 
 public class PersistenceActivator : MonoBehaviour {
 
+	public int sceneToStart = 1;
 	public int day;
 	public static PersistenceActivator instance;
 	public Text diaCalendario;
 	private StartOptions startoptions;
 	private ModalPanel ModalPanel;           //reference to the ModalPanel Class
+	private ShowPanels showPanels;
+	private Sprite icon = null;
 
 	// Use this for initialization
 	void Awake () {
 		instance = this;
 		startoptions = GetComponent<StartOptions> ();
 		ModalPanel = ModalPanel.Instance();         //Instantiate the panel
+		showPanels = GetComponent<ShowPanels> ();
 	}
 
 	public void SaveAllData() {
@@ -23,30 +27,19 @@ public class PersistenceActivator : MonoBehaviour {
 		day++;
 		newSave.day = day;
 		PersistenceHandler.SaveToFile(newSave, "save01", false);
-		Sprite icon = null;
-		ModalPanel.MessageBox(icon, "Saving data...", "All data was saved.", TestYesFunction, TestNoFunction, TestCancelFunction, TestOkFunction, false, "Ok");
+		ModalPanel.MessageBox(icon, "Saving data...", "All data was saved.", NothingFunction, NothingFunction, NothingFunction, NothingFunction, false, "Ok");
 		LoadAllData();
 	}
-	//Test function:  Do something if the "Yes" button is clicked.
-	void TestYesFunction()
-	  {
-		//
-	  }
-	//Test function:  Do something if the "No" button is clicked.
-	void TestNoFunction()
-	  {
-		//
-	  }
-	//Test function:  Do something if the "Cancel" button is clicked.
-	void TestCancelFunction()
-	  {
-		//
-	  }
-	//Test function:  Do something if the "Ok" button is clicked.
-	void TestOkFunction()
-	  {
-		//
-	  }
+	// Do nothing on ok
+	void NothingFunction()
+	{
+	//
+	}
+	void StartGameFunction()
+	{
+		startoptions.StartGameInScene();
+		//showPanels.ShowPausePanel();
+	}
 
 	/// <summary>
 	/// destroi todas as entradas da lista de saves e cria novas para cada save que for encontrado
@@ -59,16 +52,25 @@ public class PersistenceActivator : MonoBehaviour {
 			GetDataFromSave(loadedGame);
 		}
 		else {
+			ModalPanel.MessageBox(icon, "No load available", "No saved input to load \nA New Game will be started.", NothingFunction, NothingFunction, NothingFunction, NothingFunction, false, "Ok");
 			Debug.Log("There is no data to load");
 		}
 	}
 
-
 	public void LoadGame() {
 		LoadAllData();
-		startoptions.StartGameInScene();
+		StartGameFunction();
 	}
-	
+
+	public void NewGame() {
+		SavedGame loadedGame = PersistenceHandler.LoadFromFile<SavedGame>("save01");
+		if(loadedGame == null) { // no data saved - start a new game
+			StartGameFunction();
+		}
+		else { // there is a save - confirmation to button to remove data, if no: nothing, if yes: new game
+			ModalPanel.MessageBox(icon, "There is a saved file", "Do you want to start a New Game?\n\nAll data will be lost if you do!", StartGameFunction, NothingFunction, NothingFunction, NothingFunction, false, "YesNo");
+		}
+	}
 
 	public void GetDataFromSave(SavedGame theSave) {
 		day = theSave.day;
